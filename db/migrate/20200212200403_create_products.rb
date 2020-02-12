de6c634e4dcdcf236ae7d68a4777afdf3456bb52
LFM Alves <luis.projensino@gmail.com>
2020-02-12 17:08:38 -0300

    feat: changing precision of price

--- db/migrate/20200212200403_create_products.rb
+++ db/migrate/20200212200403_create_products.rb
@@ -6,7 +6,7 @@ class CreateProducts < ActiveRecord::Migration[6.0]
       t.string :title
       t.text :description
       t.string :image_url
-      t.decimal :price
+      t.decimal :price, precision: 8, scale: 2
 
       t.timestamps
     end
